d6040b1b1ab92bac81c570768a6a9e2526cf80cf
Suvigya Vijay <suvigya.vijay@flipkart.com>
2019-08-14 20:33:10 +0530

    intro

--- about.py
+++ about.py
@@ -1,5 +1,9 @@
 '''
-Hi there, I am Suvigya Vijay. And this is a big long line here.
+Hi there, I'm Suvigya. I'm a developer currently living in Bangalore, India. 
+
+Currently, I'm a Data Science Intern at Flipkart in the Planning team.
+
+While I love my work, I'm always curious about tech, anything related to computers, and interested in design. This being said I have tried out multiple domains including graphic designing, web and app development, deployment, and given a shot at numerous software tools.
 '''
 class SuvigyaVijay:
     def __init__(self):
